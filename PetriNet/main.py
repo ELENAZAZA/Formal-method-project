b3744dc8c29aace706304d7e6ff0b2798da14232
graziaperna <g.perna4@studenti.uniba.it>
2023-01-20 17:23:23 +0100

    Creato dataset e scritto codice per lettura file csv

--- PetriNet/main.py
+++ PetriNet/main.py
@@ -10,14 +10,18 @@
 # Press the green button in the gutter to run the script.
 import pandas
 import pm4py
+import os
 
 
 def import_csv(file_path):
-    event_log = pandas.read_csv(file_path, sep=',')
-    event_log = pm4py.format_dataframe(event_log, activity_key='activity', timestamp_key='timestamp')
+    event_log = pandas.read_csv(file_path, sep=';',header=1)
+    event_log = pm4py.format_dataframe(event_log, case_id='caseID', activity_key='activity', timestamp_key='timestamp')
     start_activities = pm4py.get_start_activities(event_log)
     end_activities = pm4py.get_end_activities(event_log)
     print("Start activities: {}\nEnd activities: {}".format(start_activities, end_activities))
 
 if __name__ == "__main__":
-    import_csv("dataset_log_1.CSV.csv")
+    cwd = os.getcwd()  # Get the current working directory (cwd)
+    files = os.listdir(cwd)  # Get all the files in that directory
+    print("Files in %r: %s" % (cwd, files))
+    import_csv('Formal-method-project\PetriNet\dataset.CSV')
